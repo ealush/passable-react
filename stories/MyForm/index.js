624b2d4088aed6f296eac784edfeba1ee0aa0490
Evyatar <evyatar.a@fiverr.com>
2017-12-16 12:39:46 +0200

    better-example

---
+++ stories/MyForm/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PassableProvider from '../../src/PassableProvider';
+import passes from './passes';
+
+class MyForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+            <PassableProvider name="MyForm" passes={passes}>
+                {({onChange, onBlur, fields}) => (
+                    <form>
+                        <input type="text" onChange={onChange} onBlur={onBlur} name="username"/>
+                        <input type="number" onChange={onChange} onBlur={onBlur} name="phone"/>
+                    </form>
+                )}
+            </PassableProvider>
+        );
+    }
+}
+
+export default MyForm;
--- stories/index.js
+++ stories/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import PassableProvider from '../src/index';
-import passes from './MyForm/passes';
+import MyForm from './MyForm';
 
 storiesOf('Simple Example', module)
     .add('with text', () => (
-        <PassableProvider name="MyForm" passes={passes}>
-            {({onChange, onBlur, fields}) => (
-                <form>
-                    <input type="text" onChange={onChange} onBlur={onBlur} name="username"/>
-                    <input type="number" onChange={onChange} onBlur={onBlur} name="phone"/>
-                </form>
-            )}
-        </PassableProvider>
+        <MyForm/>
     ));
