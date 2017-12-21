c22c9203a0aa331e392e6642178a123f55b17e87
Evyatar <evyatar.a@fiverr.com>
2017-12-22 01:12:32 +0200

    [Spec] test fieldAttributesByType

--- src/PassableProvider/lib/fieldAttributesByType/index.js
+++ src/PassableProvider/lib/fieldAttributesByType/index.js
@@ -3,8 +3,9 @@ const RADIO = 'radio';
 const ACCEPTING_CHECKED = [CHECKBOX, RADIO];
 
 export default function fieldAttributesByType(field) {
+    field = field || {};
     const attrs = {};
     if (ACCEPTING_CHECKED.includes(field.type)) { attrs.checked = field.checked; }
-    attrs.value = field.value;
+    attrs.value = typeof field.value === 'undefined' ? '' : field.value;
     return attrs;
 }
